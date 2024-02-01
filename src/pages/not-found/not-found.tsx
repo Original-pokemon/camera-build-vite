54e834d69f8790611df60443d0a06aa0489f2f1f
Original-pokemon <eremkin-02@mail.ru>
2024-02-01 17:10:50 +0300

    feat: add 404 page

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import './not-found.css';
 const NotFoundPage = () => (
-  <div>
-    <h1>404</h1>
-    <p>Страница не найдена</p>
+  <div className="not-found">
+    <div className="not-found__content">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__description">Oops! Страница, которую вы ищете, не найдена.</p>
+      <Link to="/" className="not-found__link">На главную</Link>
+    </div>
   </div>
 );
 
