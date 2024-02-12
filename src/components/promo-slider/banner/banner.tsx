8a41337b9666e6a02b4944ef743212e5168b6944
Original-pokemon <eremkin-02@mail.ru>
2024-02-12 15:56:19 +0300

    fix: text and bullet in promo slider

--- src/components/promo-slider/banner/banner.tsx
+++ src/components/promo-slider/banner/banner.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
 import { PromoType } from '../../../types';
-import { useAppSelector } from '../../../hooks/state';
-import { getProduct } from '../../../store/action';
 
 type BannerProps = {
   promo: PromoType;
@@ -9,18 +7,16 @@ type BannerProps = {
 
 const Banner = ({ promo }: BannerProps) => {
   const { id, previewImgWebp, previewImgWebp2x, previewImg, previewImg2x, name } = promo;
-  const product = useAppSelector((state) => getProduct(state, id));
-
 
   return (
     <div className="banner" data-testid="banner">
       <picture>
-        <source type="image/webp" srcSet={`${previewImgWebp}, ${previewImgWebp2x} 2x`} />
+        <source type="image/webp" srcSet={`${previewImgWebp}, ${previewImgWebp2x}`} />
         <img
           src={previewImg}
-          srcSet={`${previewImg2x} 2x`}
-          width="1280"
-          height="280"
+          srcSet={previewImg2x}
+          width={1280}
+          height={280}
           alt={name}
         />
       </picture>
@@ -28,7 +24,7 @@ const Banner = ({ promo }: BannerProps) => {
       <p className="banner__info">
         <span className="banner__message">Новинка!</span>
         <span className="title title--h1">{name}</span>
-        <span className="banner__text">{product?.description}</span>
+        <span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span>
         <Link className="btn" to={`/product/${id}`}>
           Подробнее
         </Link>
