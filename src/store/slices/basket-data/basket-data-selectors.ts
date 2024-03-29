d90f32a4094dc8df1d65295efcf18ae831606289
Original-pokemon <eremkin-02@mail.ru>
2024-03-29 17:29:21 +0300

    refactor: getBasketTotal return all products quantity sum

--- src/store/slices/basket-data/basket-data-selectors.ts
+++ src/store/slices/basket-data/basket-data-selectors.ts
@@ -4,9 +4,10 @@ import { basketAdapter, basketSlice } from './basket-data';
 
 type BasketStateType = Pick<InitialStateType, typeof NameSpace.Basket>
 
-export const { selectAll: getBasketItems, selectTotal: getBasketTotal, selectById: getBasketItem, selectIds: getBasketIds } = basketAdapter.getSelectors<Pick<InitialStateType, typeof NameSpace.Basket>>((state) => state[NameSpace.Basket]);
+export const { selectAll: getBasketItems, selectById: getBasketItem, selectIds: getBasketIds } = basketAdapter.getSelectors<Pick<InitialStateType, typeof NameSpace.Basket>>((state) => state[NameSpace.Basket]);
 export const { addToBasket, removeProduct, decreaseProductQuantity, increaseProductQuantity, changeProductQuantity, setBasket, resetBasket } = basketSlice.actions;
 
 export const getCoupon = (state: BasketStateType) => state[NameSpace.Basket].coupon;
+export const getBasketTotal = (state: BasketStateType) => Object.values(state[NameSpace.Basket].entities).reduce((acc, item) => acc + (item?.quantity ?? 0), 0);
 export const getCouponStatus = (state: BasketStateType) => state[NameSpace.Basket].couponPostStatus;
 export const getOrderStatus = (state: BasketStateType) => state[NameSpace.Basket].status;
